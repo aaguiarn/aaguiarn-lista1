d83b0ad8a9c46b26cb5078ea93feca77382e9361
Antônio V de Aguiar Neto <antonio.patriota@gmail.com>
2021-10-01 19:45:40 -0300

    Update 1a_lista1_3.cpp

--- 1a_lista1_3/1a_lista1_3.cpp
+++ 1a_lista1_3/1a_lista1_3.cpp
@@ -1,3 +1,9 @@
+//Lista de atividade de Linguagem de Programação 2021.1
+//Disciplina Ministrada em 2021.1 Pelo Prof Caio
+//Implementação dos Scripts por Antônio Aguiar - Engenharia Elétrica
+//1a_lista1_3.cpp : Este arquivo contém a função 'main'.
+//A execução do programa começa e termina nesse script.
+
 #include<iostream>
 using namespace std;
 
